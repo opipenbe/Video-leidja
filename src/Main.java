b229c37b452b4e3d7d7b576caaa25c498047aa40
opipenbe <olari.pipenberg@gmail.com>
2014-12-09 14:36:38 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,11 @@ import java.io.IOException;
 /**
  * Peameetodit sisaldav class.
  *
- * @version 0.1
+ * @version 0.3
  * @author Olari Pipenberg
  * @since 1.7
  */
-/*
- * Proovimiseks: http://kanal2.ee/pluss/closed?id=23523
- * http://www.liveleak.com/view?i=671_1417498779
- * http://arhiiv.err.ee/vaata/etv-kuld-pop-3
- * http://kanal2.ee/pluss/closed?id=23662
- * http://arhiiv.err.ee/vaata/see-vana-hea-dixi/latest
- * http://alfabass.at.ua/index/temptation/0-32
- */
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
